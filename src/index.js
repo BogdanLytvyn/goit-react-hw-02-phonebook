14a09c1311a30255d18049aab169b568ba01f490
Bogdan Lytvyn <72852244+BogdanLytvyn@users.noreply.github.com>
2021-02-26 19:35:46 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'modern-normalize/modern-normalize.css';
 import './globalize.css';
-import App from './componets/App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
